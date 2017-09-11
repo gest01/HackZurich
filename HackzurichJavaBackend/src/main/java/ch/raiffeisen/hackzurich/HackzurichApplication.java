60ba8d9622ad8bab65ad7b3163c98987826e1ae4
Simon Knupfer <simon@knupfer.eu>
2017-09-11 19:35:03 +0200

    Java Backend

---
+++ HackzurichJavaBackend/src/main/java/ch/raiffeisen/hackzurich/HackzurichApplication.java
@@ -0,0 +1,12 @@
+package ch.raiffeisen.hackzurich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackzurichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackzurichApplication.class, args);
+	}
+}
