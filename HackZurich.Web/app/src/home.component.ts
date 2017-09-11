e1403446f2b9c2e3c2c19d9844ff59138ea21d3e
Simon Knupfer <simon@knupfer.eu>
2017-09-11 18:08:57 +0200

    Checkin Test Simon

--- HackZurich.Web/app/src/home.component.ts
+++ HackZurich.Web/app/src/home.component.ts
@@ -1,10 +1,17 @@
 import * as Rx from "rxjs";
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {importExpr} from "@angular/compiler/src/output/output_ast";
 
 @Component({
     selector: "home",
     templateUrl: "home.component.html",
 })
-export class HomeComponent {
+export class HomeComponent implements  OnInit{
+
+    helloWorldText: String;
+
+    ngOnInit(): void {
+        this.helloWorldText = "Hello by Simon"
+    }
 
 }
