73601af817f7b7de8adff37a8ff45c9e8ed252af
Stefan Geiger <stefan.geiger@outlook.com>
2017-09-13 20:14:09 +0200

    backend code removed

--- HackzurichJavaBackend/src/main/resources/db/migration/V1__create_tables.sql
+++
@@ -1,5 +0,0 @@
-create table Person (
-  id bigint not null primary key identity(1,1),
-  vorname varchar(255),
-  nachname varchar(255)
-);
--- HackzurichJavaBackend/src/main/resources/db/migration/V2__insertData.sql
+++
@@ -1,3 +0,0 @@
-insert into Person (vorname, nachname) values ('Harry', 'Hirsch');
-insert into Person (vorname, nachname) values ('Tante', 'Emma');
-insert into Person (vorname, nachname) values ('Franz', 'Klammer');
